22feefb486991224a03c237691da41c68bd33d53
Ross Delinger <rossdylan@csh.rit.edu>
2012-05-11 17:08:06 -0400

    Removed traces of old functions, and finish refactoring things

--- gibsy/__init__.py
+++ gibsy/__init__.py
@@ -5,6 +5,8 @@ import os
 import json
 from fapws import base
 from daemon import Daemon
+import utilities as util
+
 
 DEFAULT_CONFIG = {
         "blog_title": "New Gibsy Blog",
@@ -16,49 +18,6 @@ DEFAULT_CONFIG = {
         "blog_url": "gibsy.awesome-sauce.com",
         "blog_desc": "Fuck yah Gibsy"}
 
-
-"""Installer functions"""
-def generate_git_hook(config_path):
-    return "#!/bin/bash\ngibsy.py restart %s" % config_path
-
-def install():
-    cwd = os.getcwd()
-    install_here = yorn("Install to current directory?")
-    if install_here:
-        install_path = cwd
-    else:
-        install_path = raw_input("Enter an install path; ")
-    blog_name = raw_input("Enter a blog name: ")
-    os.chdir(install_path)
-    print "Creating Git Repository"
-    git_repo = os.path.join(install_path,blog_name + ".git")
-    os.mkdir(git_repo)
-    os.chdir(git_repo)
-    run_command("git init --bare")
-    os.chdir(install_path)
-    print "Cloning Git Repository"
-    run_command("git clone %s" % git_repo)
-    git_clone = os.path.join(install_path,blog_name)
-    print "Creating Gibsy File Structure"
-    os.chdir(git_clone)
-    os.mkdir("posts")
-    run_command("touch posts/first.post")
-    config_path = os.path.join(git_clone, "gibsy.conf")
-    git_hook = generate_git_hook(config_path)
-    with open("gibsy.conf", "w") as f:
-        DEFAULT_CONFIG.update({"git_repo": git_repo, "git_clone": git_clone, "pid_path": os.path.join(git_clone,"gibsy.pid")})
-        f.write(json.dumps(DEFAULT_CONFIG))
-    print "Commiting and pushing fle structure"
-    run_command("git add posts/first.post gibsy.conf")
-    run_command("git commit -m 'initial commit'")
-    run_command("git push origin master")
-    print "Creating git hook"
-    os.chdir(git_repo)
-    with open("hooks/post-receive","w") as f:
-        f.write(git_hook)
-    os.chdir(cwd)
-
-"""Blog Data classes"""
 
 
 
@@ -79,10 +38,11 @@ class Server(Daemon):
     def update(self):
         current_dir = os.getcwd()
         os.chdir(self.config['git_clone'])
-        run_command("git pull origin master")
+        util.run_command("git pull origin master")
         os.chdir(current_dir)
 
     def run(self):
+        from blog import Blog
         self.update()
         self.blog = Blog(self.config)
         evwsgi.start(self.config['host'], self.config['port'])
@@ -92,6 +52,7 @@ class Server(Daemon):
 
 def gibsy_main():
     import sys
+    from installer import install
     if len(sys.argv) == 3:
         command = sys.argv[1]
         config_file = sys.argv[2]
